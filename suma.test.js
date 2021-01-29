c197c2110a4e61b8debe84ad1dc484b095a5805c
MiguDeveloper <miguel.peru.seo@gmail.com>
2021-01-28 22:56:53 -0500

    create workflow

--- suma.test.js
+++ suma.test.js
@@ -1,5 +1,9 @@
 const sum = require('./suma');
 
-test('sumar 1 + 2 igual 3', () => {
+test('1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('a + 5 = NaN', () => {
+  expect(sum('a', 5)).toBe(NaN);
 });
